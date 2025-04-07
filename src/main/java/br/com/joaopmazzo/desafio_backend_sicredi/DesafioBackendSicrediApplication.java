291a6d5650e6dd3bb622723c903bdbf741824a65
João Paulo Mazzo <mazzojp@gmail.com>
2025-04-07 20:11:27 -0300

    configuraçoes iniciais
    cria o projeto e configura as dependencias julgadas necessárias assim como
    o compose do docker com o banco de dados

---
+++ src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/DesafioBackendSicrediApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopmazzo.desafio_backend_sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendSicrediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaopmazzo/desafio_backend_sicredi/DesafioBackendSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopmazzo.desafio_backend_sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
