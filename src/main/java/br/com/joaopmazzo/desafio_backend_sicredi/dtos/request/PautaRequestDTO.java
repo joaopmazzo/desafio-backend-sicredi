6d4b8e07b81e09f60222ee9fc457856942edf658
João Paulo Mazzo <mazzojp@gmail.com>
2025-04-08 23:02:29 -0300

    feat: refatora PautaRequestDTO para utilizar record e simplificar a estrutura

--- src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/dtos/request/PautaRequestDTO.java
+++ src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/dtos/request/PautaRequestDTO.java
@@ -2,16 +2,12 @@ package br.com.joaopmazzo.desafio_backend_sicredi.dtos.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
-public class PautaRequestDTO {
-
+public record PautaRequestDTO(
     @NotBlank(message = "O título não pode estar em branco.")
     @Size(max = 255, message = "O título deve ter no máximo 255 caracteres.")
-    private String titulo;
+    String titulo,
 
     @NotBlank(message = "A descrição não pode estar em branco.")
-    private String descricao;
-
-}
+    String descricao
+) {}
