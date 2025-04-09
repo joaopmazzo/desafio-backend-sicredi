911f50cce702ae3618e8abf902944e0e07bc61c1
João Paulo Mazzo <mazzojp@gmail.com>
2025-04-08 22:56:55 -0300

    feat: habilita agendamento de execução ao adicionar anotação @EnableScheduling

--- src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/DesafioBackendSicrediApplication.java
+++ src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/DesafioBackendSicrediApplication.java
@@ -2,8 +2,10 @@ package br.com.joaopmazzo.desafio_backend_sicredi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DesafioBackendSicrediApplication {
 
 	public static void main(String[] args) {
