02a76bf3adaf508c3efaa7309d264168e4b856d1
João Paulo Mazzo <mazzojp@gmail.com>
2025-04-10 13:06:03 -0300

    refactor: troca de record para classe para poder ter um default da duração

--- src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/application/dtos/request/SessaoRequestDTO.java
+++ src/main/java/br/com/joaopmazzo/desafio_backend_sicredi/application/dtos/request/SessaoRequestDTO.java
@@ -1,14 +1,19 @@
 package br.com.joaopmazzo.desafio_backend_sicredi.application.dtos.request;
 
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record SessaoRequestDTO(
-        @Positive(message = "O tempo de duração deve ser um valor positivo.")
-        Integer duracaoEmMinutos
-) {
-    public SessaoRequestDTO {
-        if (duracaoEmMinutos == null) {
-            duracaoEmMinutos = 1;
-        }
-    }
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessaoRequestDTO {
+
+    @Positive(message = "O tempo de duração deve ser um valor positivo.")
+    @Builder.Default
+    private Integer duracaoEmMinutos = 1;
+
 }
