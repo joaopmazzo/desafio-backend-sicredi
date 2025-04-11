5a7672e45a3c544f7a58f87fe961e5628d88302a
João Paulo Mazzo <mazzojp@gmail.com>
2025-04-11 14:12:01 -0300

    test: refatora RegisterVotoUseCaseTest ajustando a verificação de exceções

--- src/test/java/br/com/joaopmazzo/desafio_backend_sicredi/application/usecases/RegisterVotoUseCaseTest.java
+++ src/test/java/br/com/joaopmazzo/desafio_backend_sicredi/application/usecases/RegisterVotoUseCaseTest.java
@@ -50,13 +50,13 @@ public class RegisterVotoUseCaseTest {
     void shouldRegisterVoteSuccessfully() {
         UUID pautaId = UUID.randomUUID();
         VotoRequestDTO requestDTO = new VotoRequestDTO("12345678900", EscolhaVotoEnum.SIM);
-        SessaoEntity sessaoEntity = new SessaoEntity();
         AssociadoEntity associadoEntity = AssociadoEntity.builder().ableToVote(true).build();
+        SessaoEntity sessaoEntity = new SessaoEntity();
         VotoEntity savedVotoEntity = new VotoEntity();
         VotoResponseDTO responseDTO = new VotoResponseDTO();
 
-        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         when(associadoService.validateAssociado(requestDTO.documento())).thenReturn(associadoEntity);
+        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         doNothing().when(votoService).alreadyVoted(sessaoEntity, associadoEntity);
         when(votoService.saveVoto(any(VotoEntity.class))).thenReturn(savedVotoEntity);
         when(votoMapper.toResponseDTO(savedVotoEntity)).thenReturn(responseDTO);
@@ -64,24 +64,11 @@ public class RegisterVotoUseCaseTest {
         VotoResponseDTO result = registerVotoUseCase.execute(pautaId, requestDTO);
 
         assertEquals(responseDTO, result);
-        verify(sessaoService).validateSessao(pautaId);
         verify(associadoService).validateAssociado(requestDTO.documento());
+        verify(sessaoService).validateSessao(pautaId);
         verify(votoService).alreadyVoted(sessaoEntity, associadoEntity);
         verify(votoService).saveVoto(any(VotoEntity.class));
         verify(votoMapper).toResponseDTO(savedVotoEntity);
-    }
-
-    @Test
-    @DisplayName("Should throw exception when session is closed")
-    void shouldThrowExceptionWhenSessionIsInvalid() {
-        UUID pautaId = UUID.randomUUID();
-        VotoRequestDTO requestDTO = new VotoRequestDTO("12345678900", EscolhaVotoEnum.NAO);
-
-        when(sessaoService.validateSessao(pautaId)).thenThrow(new SessaoEncerradaException());
-
-        assertThrows(SessaoEncerradaException.class, () -> registerVotoUseCase.execute(pautaId, requestDTO));
-        verify(sessaoService).validateSessao(pautaId);
-        verifyNoInteractions(associadoService, votoService, votoMapper);
     }
 
     @Test
@@ -89,15 +76,12 @@ public class RegisterVotoUseCaseTest {
     void shouldThrowExceptionWhenAssociadoNotFound() {
         UUID pautaId = UUID.randomUUID();
         VotoRequestDTO requestDTO = new VotoRequestDTO("12345678900", EscolhaVotoEnum.SIM);
-        SessaoEntity sessaoEntity = new SessaoEntity();
 
-        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         when(associadoService.validateAssociado(requestDTO.documento())).thenThrow(new AssociadoNotFoundException());
 
         assertThrows(AssociadoNotFoundException.class, () -> registerVotoUseCase.execute(pautaId, requestDTO));
-        verify(sessaoService).validateSessao(pautaId);
         verify(associadoService).validateAssociado(requestDTO.documento());
-        verifyNoInteractions(votoService, votoMapper);
+        verifyNoInteractions(sessaoService, votoService, votoMapper);
     }
 
     @Test
@@ -105,14 +89,27 @@ public class RegisterVotoUseCaseTest {
     void shouldThrowExceptionWhenAssociadoNotAllowedToVote() {
         UUID pautaId = UUID.randomUUID();
         VotoRequestDTO requestDTO = new VotoRequestDTO("12345678900", EscolhaVotoEnum.NAO);
-        SessaoEntity sessaoEntity = new SessaoEntity();
 
-        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         when(associadoService.validateAssociado(requestDTO.documento())).thenThrow(AssociadoNaoPodeVotarException.class);
 
         assertThrows(AssociadoNaoPodeVotarException.class, () -> registerVotoUseCase.execute(pautaId, requestDTO));
-        verify(sessaoService).validateSessao(pautaId);
         verify(associadoService).validateAssociado(requestDTO.documento());
+        verifyNoInteractions(sessaoService, votoService, votoMapper);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when session is closed")
+    void shouldThrowExceptionWhenSessionIsInvalid() {
+        UUID pautaId = UUID.randomUUID();
+        VotoRequestDTO requestDTO = new VotoRequestDTO("12345678900", EscolhaVotoEnum.NAO);
+        AssociadoEntity associadoEntity = new AssociadoEntity();
+
+        when(associadoService.validateAssociado(requestDTO.documento())).thenReturn(associadoEntity);
+        when(sessaoService.validateSessao(pautaId)).thenThrow(new SessaoEncerradaException());
+
+        assertThrows(SessaoEncerradaException.class, () -> registerVotoUseCase.execute(pautaId, requestDTO));
+        verify(associadoService).validateAssociado(requestDTO.documento());
+        verify(sessaoService).validateSessao(pautaId);
         verifyNoInteractions(votoService, votoMapper);
     }
 
@@ -124,13 +121,13 @@ public class RegisterVotoUseCaseTest {
         SessaoEntity sessaoEntity = new SessaoEntity();
         AssociadoEntity associadoEntity = new AssociadoEntity();
 
-        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         when(associadoService.validateAssociado(requestDTO.documento())).thenReturn(associadoEntity);
+        when(sessaoService.validateSessao(pautaId)).thenReturn(sessaoEntity);
         doThrow(new VotoJaRegistradoException()).when(votoService).alreadyVoted(sessaoEntity, associadoEntity);
 
         assertThrows(VotoJaRegistradoException.class, () -> registerVotoUseCase.execute(pautaId, requestDTO));
-        verify(sessaoService).validateSessao(pautaId);
         verify(associadoService).validateAssociado(requestDTO.documento());
+        verify(sessaoService).validateSessao(pautaId);
         verify(votoService).alreadyVoted(sessaoEntity, associadoEntity);
         verifyNoInteractions(votoMapper);
     }
